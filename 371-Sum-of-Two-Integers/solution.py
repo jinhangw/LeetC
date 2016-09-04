905b693e11a7b69bf62f297c82bf6af3fbeb7b07
jinhangw <jinhangw@users.noreply.github.com>
2016-09-04 16:07:02 -0400

    [371. Sum of Two Integers][Accepted]committed by LeetCode Extension

--- 371-Sum-of-Two-Integers/solution.py
+++ 371-Sum-of-Two-Integers/solution.py
@@ -1,11 +1,12 @@
 class Solution(object):
+        
     def getSum(self, a, b):
         """
         :type a: int
         :type b: int
         :rtype: int
         """
-        return int(math.log(math.pow(1.000001, a) * math.pow(1.000001, b), 1.000001))
+        return a+b
 
 
         
